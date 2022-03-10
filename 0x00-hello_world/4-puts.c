b052fcd39f21788b086c70f9fc5ad1a0508150e7
Zeelinga <zeelingazee@gmail.com>
2022-03-10 12:22:04 -0800

    4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - This prints out the text when run
+ *
+ * Return: 0 without errors
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
