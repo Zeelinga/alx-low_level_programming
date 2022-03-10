8deb82eba4f48d1b07992b359cf627eb6366d677
Zeelinga <zeelingazee@gmail.com>
2022-03-10 14:00:13 -0800

    101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 
-  /**
-   * main - This should successfully print out the text
-   *
-   * Return: 1 if there are no errors
-   */
+/**
+  * main - This should successfully print out the text
+  *
+  * Return: 1 if there are no errors
+  */
 int main(void)
 {
 	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
