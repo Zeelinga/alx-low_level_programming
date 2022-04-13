53e2957b790c90c60c90d22954b2404bc9377c39
Zeelinga <zeelingazee@gmail.com>
2022-04-13 15:22:57 -0700

    A goal is not always meant to be reached

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,46 +1,39 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 #include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
 /**
- * main - main
- * @argc: arg count
- * @argv: arg array
- *
- * Return: int
- *
- **/
-int main(int argc, char **argv)
+* main - Prints the result of simple operations.
+* @argc: The number of arguments supplied to the program.
+* @argv: An array of pointers to the arguments.
+*
+* Return: Always 0.
+*/
+int main(int argc, char *argv[])
 {
-	int inta, intb;
-	int (*f)(int, int);
+	int num1, num2;
+	char *op;
 
 	if (argc != 4)
 	{
 		printf("Error\n");
-		return (98);
+		exit(98);
 	}
 
-	f = get_op_func(argv[2]);
+	num1 = atoi(argv[1]);
+	op = argv[2];
+	num2 = atoi(argv[3]);
 
-	if (f == NULL)
-	{
-		printf("Error\n");
-		return (99);
-	}
-
-	if ((argv[2][0] != '+' && argv[2][0] != '-' && argv[2][0] != '/' &&
-	      argv[2][0] != '*' && argv[2][0] != '%') || strlen(argv[2]) != 1)
+	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
-
-		inta = atoi(argv[1]);
-		intb = atoi(argv[3]);
-
-		printf("%d\n", f(inta, intb));
-
-		return (0);
-
+	if ((*op == '/' && num2 == 0) ||
+	    (*op == '%' && num2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", get_op_func(op)(num1, num2));
+	return (0);
 }
